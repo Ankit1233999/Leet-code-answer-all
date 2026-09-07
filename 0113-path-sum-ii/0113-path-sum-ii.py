b7725b9ee427de71295b3e81066fdd83c5aa5072
Ankit saini1707 <sainiankit6244@gmail.com>
2026-09-07 22:14:46 +0530

    Time: 2 ms (72.53%), Space: 15.3 MB (35.91%) - LeetHub

---
+++ 0113-path-sum-ii/0113-path-sum-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def pathSum(self, root, targetSum):
+        result = []
+
+        def dfs(node, remaining, path):
+            if node is None:
+                return
+
+            path.append(node.val)
+            remaining -= node.val
+
+            
+            if node.left is None and node.right is None:
+                if remaining == 0:
+                    result.append(path[:])
+            else:
+                dfs(node.left, remaining, path)
+                dfs(node.right, remaining, path)
+
+          
+            path.pop()
+
+        dfs(root, targetSum, [])
+        return result
